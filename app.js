7eb4efd4becd5146486cae5815b19981f31781bf
ZaidJamo <>
2021-07-25 10:41:58 +0000

    updated app

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ alert("hey, wlecome to my website " + name )
 var showType = prompt("what type of shows do you like :D(action, comedy, mind bending)")
 
 
-while(showType != "action" && favCar != "comedy" && favCar != "mind bending")
+while(showType != "action" && showType != "comedy" && showType != "mind bending")
 
 showType = prompt("what kind do you want to watch? , please choose one of these ; action, comedy, mind bending")
 
@@ -26,13 +26,13 @@ else if (showType =="comedy"){
 for(var i = 0; i<numOfT; i++){
 document.write(
   "<div>" + "<h3>" + showType +"</h3>" + "bojack horseman:  "
-+ "<img  src='https://i.pinimg.com/474x/71/c6/5d/71c65d8fd0d7f6eaf5e699a9bd69b126.jpg' width='100' height='150'     />" + "</div>");}
++ "<img  src='https://i.pinimg.com/474x/71/c6/5d/71c65d8fd0d7f6eaf5e699a9bd69b126.jpg' width='100' height='150'     />" + "</div>")}
 }
 else if(showType =="mind bending"){
 for(var i = 0; i<numOfT; i++){
 document.write(
   "<div>" + "<h3>" + showType +"</h3>" + "love death + robots:  "
-+ "<img  src='https://m.media-amazon.com/images/M/MV5BYjEwOWQ0MjktMjZjNy00Mzc1LWE5NTItMDQ1Yjc0Zjk0NTBlXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg' width='100' height='150'     />" + "</div>");}
++ "<img  src='https://m.media-amazon.com/images/M/MV5BYjEwOWQ0MjktMjZjNy00Mzc1LWE5NTItMDQ1Yjc0Zjk0NTBlXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg' width='100' height='150'     />" + "</div>")}
 }
 else{
   alert('no Recommendation for this genre');
